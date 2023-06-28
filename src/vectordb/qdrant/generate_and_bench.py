9903963172fbe2dc131acf200eaf5082cf6eb8fb
Malcolm Greaves <greaves.malcolm@gmail.com>
2023-06-28 12:50:23 -0400

    starting benchmark for qdrant

---
+++ src/vectordb/qdrant/generate_and_bench.py
@@ -0,0 +1,14 @@
+"""Benchmarking embedding generation and storage, retrieval, and kNN with qdrant."""
+
+from typing import Sequence
+
+__all__: Sequence[str] = ("bench_qdrant_full",)
+
+
+def bench_qdrant_full() -> None:
+    """Qdrant benchmarking."""
+    raise NotImplementedError
+
+
+if __name__ == "__main__":
+    bench_qdrant_full()
